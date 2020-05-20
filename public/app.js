ae285452150430114affead13247f3bee66ca127
rikyperdana <perdanariky@gmail.com>
2020-05-20 16:36:06 +0700

    find all

--- public/app.js
+++ public/app.js
@@ -1,8 +1,40 @@
-const poster = (url, obj, cb) => $.post(
+var state = {},
+poster = (url, obj, cb) => $.post(
   url, JSON.stringify(obj), res => cb(JSON.parse(res))
+),
+
+makeModal = name => m('.modal',
+  {class: state[name] && 'is-active'},
+  m('.modal-background'),
+  m('.modal-content', state[name]),
+  m('.modal-close.is-large', {onclick: () =>
+    [state[name] = null, m.redraw()]
+  })
 )
 
-m.mount(document.body, {view: () => m('.content',
-  {oncreate: () => poster('/test', {b: 2}, console.log)},
-  m('h1', 'Hello Deno')
-)})
+m.mount(document.body, {view: () => m('.container', m('.content',
+  m('h1', 'Hello Deno'),
+  m('.button.is-primary', {
+    onclick: () => state.modalGetCollection = m('.box',
+      m(autoForm({
+        id: 'getCollection', schema: {
+          dbName: {type: String, label: 'Database Name'},
+          collName: {type: String, label: 'Collection Name'}
+        }, action: doc => [
+          poster('dbCall', doc, res => [
+            state.collData = res.data,
+            m.redraw()
+          ]),
+          state.modalGetCollection = null
+        ]
+      }))
+    )
+  }, 'Get Collection'),
+  makeModal('modalGetCollection'),
+  m('table.table',
+    m('thead', m('tr', m('th', '_id'))),
+    m('tbody', (state.collData || []).map(i => m('tr',
+      m('td', i._id)
+    )))
+  )
+))})
--- server.js
+++ server.js
@@ -5,8 +5,7 @@ import 'https://deno.land/x/dotenv/load.ts'
 await init()
 var client = new MongoClient()
 client.connectWithUri(Deno.env.get('MONGO'))
-var db = client.database('test'),
-people = db.collection('users')
+// people = db.collection('users')
 // people.find({}).then(res => console.log(res))
 
 const responder = obj => ({
@@ -16,9 +15,12 @@ const responder = obj => ({
 
 app = createApp()
 app.use(serveStatic('./public'))
-app.post('/test', async req => {
-  var text = await req.text()
-  req.respond(responder({a: 1}))
+app.post('/dbCall', async req => {
+  var text = await req.text(), obj = JSON.parse(text),
+  db = client.database(obj.dbName),
+  coll = db.collection(obj.collName),
+  list = await coll.find({})
+  req.respond(responder({data: list}))
 })
 
 app.listen({port: 3000})
