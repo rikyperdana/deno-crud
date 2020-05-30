8c0f85497412aa1aec774c1e43172fd451a53065
rikyperdana <perdanariky@gmail.com>
2020-05-30 23:25:34 +0700

    back to 1.0

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import {createApp, serveStatic} from 'https://servestjs.org/@v1.1.0/mod.ts'
+import {createApp, serveStatic} from 'https://servestjs.org/@v1.0.0/mod.ts'
 import {init, MongoClient} from 'https://deno.land/x/mongo@v0.6.0/mod.ts'
 import 'https://deno.land/x/dotenv/load.ts'
 
